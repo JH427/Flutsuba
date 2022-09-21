5286b1a6ba2a8988a4bcb4a1bb217eb98434c5a5
JH427 <jeharper@uga.edu>
2022-09-21 00:17:05 -0400

    verification issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutsuba/views/login_view.dart';
 import 'package:flutsuba/views/register_view.dart';
+import 'package:flutsuba/views/verify_email_view.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 
@@ -33,14 +35,18 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // print(user);
-            // if (user?.emailVerified ?? false) {
-            //   return const Text("Done");
-            // } else {
-            //   return const VerifyEmailView();
-            // }
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            print(user);
+            if (user != null) {
+              if (user.emailVerified) {
+                print("Email verified");
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text("Done");
           default:
             return const CircularProgressIndicator();
         }
